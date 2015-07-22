4e4ac583a6eb7526b700849e751ee69ec7013147
Hiroyuki Miyazaki <heromiya@heromiyadebian>
2015-07-22 13:43:15 +0900

    modified:   exec.sh 	modified:   template.map

--- exec.sh
+++ exec.sh
@@ -14,11 +14,12 @@ for ARG in `cat ROI.lst`; do
 	export YMIN=`echo $ARG | cut -f 3 -d '|'`
 	export XMAX=`echo $ARG | cut -f 4 -d '|'`
 	export YMAX=`echo $ARG | cut -f 5 -d '|'`
-    for FILE in `ls ngdc.noaa.gov | grep \.tif$`; do
+    for FILE in `ls ngdc.noaa.gov | grep -e \.tif$ | grep -v -e web.stable_lights.avg_vis.`; do
 	export FILE
 	make maps/${ROI}_${XMIN}_${YMIN}_${XMAX}_${YMAX}.$FILE.png
     done 
-    for PRODUCT in .stable_lights.avg_lights_x_pct _web.stable_lights.avg_vis; do 
+# _web.stable_lights.avg_vis
+    for PRODUCT in .stable_lights.avg_lights_x_pct; do 
 	export PRODUCT
 	make pdf/${ROI}_${XMIN}_${YMIN}_${XMAX}_${YMAX}${PRODUCT}.pdf
     done
